f54a7a13cbca518ae9dbe68c21d5cbfcc532fdac
Stupishin Daniil <stupishin1597@yandex.ru>
2024-04-28 12:44:25 +0300

    version 0.1.0

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // app.js
 const express = require("express");
-
-// Импортируем роутеры
+const path = require("path");
 const mainRoute = require("./routes/main");
 const gamesRouter = require("./routes/games");
 
 const PORT = 3000;
 const app = express();
 
-// Запускаем
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(mainRoute, gamesRouter);
 
 app.listen(PORT, () => {
--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,9 @@
 const mainRoute = require("express").Router();
 
 mainRoute.get("/", (req, res) => {
-  res.status(200).send("<h1>Скоро тут будет интерфейс</h1>");
+  fs.readFile("./public/index.html", "utf-8").then((data) => {
+    res.header("Content-Type", "text/html").send(data);
+  });
 });
 
 module.exports = mainRoute;
