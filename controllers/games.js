10682475fdd86b4ecd53b775c1756a336f99cfad
Stupishin Daniil <stupishin1597@yandex.ru>
2024-04-28 22:00:06 +0300

    version 1.1

--- controllers/games.js
+++ controllers/games.js
@@ -1,70 +1,12 @@
-const { writeData } = require("../utils/data"); // Чтение и запись данных в JSON-файл
-
-const addGameController = async (req, res) => {
-  // Проверяем, есть ли уже в списке игра с таким же названием
-  req.isNew = !Boolean(req.games.find((item) => item.title === req.body.title));
-  // Если игра, которую хотим добавить, новая (её не было в списке)
-  if (req.isNew) {
-    // Добавляем объект с данными о новой игре
-    const inArray = req.games.map((item) => Number(item.id));
-    let maximalId;
-    if (inArray.length > 0) {
-      maximalId = Math.max(...inArray);
-    } else {
-      maximalId = 0;
-    }
-    req.updatedObject = {
-      id: maximalId + 1,
-      title: req.body.title,
-      image: req.body.image,
-      link: req.body.link,
-      description: req.body.description,
-    };
-    // Добавляем данные о новой игре в список с другими играми
-    req.games = [...req.games, req.updatedObject];
-  } else {
-    res.status(400);
-    res.send({ status: "error", message: "Игра с таким именем уже есть." });
-    return;
-  }
-  // Записываем обновлённый список игр в файл
-  await writeData("./data/games.json", req.games);
-  // В качестве ответа отправляем объект с двумя полями
-  res.send({
-    games: req.games, // Обновлённый список со всеми играми
-    updated: req.updatedObject, // Новая добавленная игра
-  });
-};
-// Получим игры из JSON файла и отправим в ответ на запрос
-const sendAllGames = async (req, res) => {
+const sendAllGames = (req, res) => {
   res.send(req.games);
 };
 
-const deleteGame = async (req, res) => {
-  // Прочитаем запрашиваемый id игры из запроса
-  const id = Number(req.params.id);
-
-  // Найдём игру, которую хотят удалить из общего массива с играми, по id
-  req.game = req.games.find((item) => item.id === id);
-
-  // Найдём индекс удаляемой игры в общем массиве игр
-  const index = req.games.findIndex((item) => item.id === req.game.id);
-
-  // Удалим из массива игр игру
-  req.games.splice(index, 1);
-
-  // Запишем обновленный массив игр в JSON файл
-  await writeData("./data/games.json", req.games);
-
-  // Вернём ответ о проделанной операции с данными о играх
+const sendUpdatedGames = (req, res) => {
   res.send({
     games: req.games,
-    updated: req.game,
+    updated: req.updatedObject,
   });
 };
 
-module.exports = {
-  sendAllGames,
-  deleteGame,
-  addGameController,
-};
+module.exports = { sendAllGames, sendUpdatedGames };
--- routes/games.js
+++ routes/games.js
@@ -1,14 +1,33 @@
-// routes/games.js
-const gamesRouter = require("express").Router(); // Создали роутер
-const {
-  sendAllGames,
-  deleteGame,
-  addGameController,
-} = require("../controllers/games"); // Контроллеры
-const { getAllGames } = require("../middlewares/games"); // Мидлеварс
+const gamesRouter = require("express").Router();
 
-gamesRouter.post("/games", getAllGames, addGameController);
+const {
+  getAllGames,
+  checkIsTitleInArray,
+  updateGamesArray,
+  updateGamesFile,
+  findGameById,
+  deleteGame,
+} = require("../middlewares/games");
+const { sendAllGames, sendUpdatedGames } = require("../controllers/games");
+
 gamesRouter.get("/games", getAllGames, sendAllGames);
-gamesRouter.delete("/games/:id", getAllGames, deleteGame);
+
+gamesRouter.post(
+  "/games",
+  getAllGames,
+  checkIsTitleInArray,
+  updateGamesArray,
+  updateGamesFile,
+  sendUpdatedGames
+);
+
+gamesRouter.delete(
+  "/games/:id",
+  getAllGames,
+  findGameById,
+  deleteGame,
+  updateGamesFile,
+  sendUpdatedGames
+);
 
 module.exports = gamesRouter;
